b28339d75adcd97f64e9bb4b379a9380f16fc2cc
遠藤 直弥 <nendo@mba.local>
2015-02-10 00:48:02 +0900

    Initial Commit

---
+++ HospitalMapA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HospitalMapA
+//
+//  Created by 遠藤 直弥 on 2015/02/10.
+//  Copyright (c) 2015年 Greenpeas Info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
