09a2b33ac024ba056a78d9186af093fae8ac6074
nendo <nendo4869@gmail.com>
2015-03-06 22:18:29 +0900

    Get the present location after starting the application immediately.

--- HospitalMapA/ViewController.swift
+++ HospitalMapA/ViewController.swift
@@ -8,22 +8,26 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var lm: CLLocationManager! = nil
+    var longitude: CLLocationDegrees!  // 緯度を保持
+    var latitude: CLLocationDegrees!  // 経度を保持
 
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // 1. 初期位置と表示範囲を設定して，画面に地図を表示する
-        // .1 初期位置を南森町駅 (34.697699,135.511078) に設定する
-        var centerCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(34.697699,135.511078)
-        // .2 表示範囲を設定する
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        // .3 表示する中心位置と範囲を設定する
-        var centerPosition = MKCoordinateRegionMake(centerCoordinate, span)
-        // .4 地図を表示する
-        mapView.setRegion(centerPosition, animated: true)
+        // .0 現在地を取得する
+        lm = CLLocationManager()
+        longitude = CLLocationDegrees()
+        latitude = CLLocationDegrees()
+        lm.delegate = self
+        lm.requestAlwaysAuthorization()
+        lm.startUpdatingLocation()
         
         // 2. オープンデータを読み込み，地図上にピンを打つ
         // http://www.city.osaka.lg.jp/contents/wdu090/opendata/mapnavoskdat_csv/mapnavoskdat_iryou.csv
@@ -65,6 +69,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // 位置情報取得成功時
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        latitude = newLocation.coordinate.latitude
+        longitude = newLocation.coordinate.longitude
+        NSLog("latitude: \(latitude) , longitude: \(longitude)")
+
+        // .1 初期位置を現在地に設定する
+        var centerCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        // .2 表示範囲を設定する
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        // .3 表示する中心位置と範囲を設定する
+        var centerPosition = MKCoordinateRegionMake(centerCoordinate, span)
+        // .4 地図を表示する
+        mapView.setRegion(centerPosition, animated: true)
+        // .5 位置情報の取得を終了する
+        lm.stopUpdatingLocation()
+    
+    }
+
+    // 位置情報取得失敗時
+    func locationManager(manager: CLLocation, didFailWithError error: NSError) {
+        NSLog("Error")
+
+        // .1 初期位置を南森町駅 (34.697699,135.511078) に設定する
+        var centerCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        // .2 表示範囲を設定する
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        // .3 表示する中心位置と範囲を設定する
+        var centerPosition = MKCoordinateRegionMake(centerCoordinate, span)
+        // .4 地図を表示する
+        mapView.setRegion(centerPosition, animated: true)
+        // .5 位置情報の取得を終了する
+        lm.stopUpdatingLocation()
+
+    }
 
 }
 
